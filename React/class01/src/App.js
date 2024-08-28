4982cad276e72bae624bf945284590a9680579cf
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-08-28 22:28:06 +0500

    portfolio

---
+++ React/class01/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+      <header className="App-header">
+        
+        <p>
+         Hello world!!
+        </p>
+        
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/class01/src/Header.js
@@ -0,0 +1,18 @@
+
+
+function Header() {
+return(
+
+    <div className="menu"> 
+
+    <a href="#" >  Home </a>
+    <a href="#" >  About </a>
+    <a href="#" >  contact </a>
+    <a href="#" >  product </a>
+    </div>
+
+)
+
+}
+
+export default Header;
