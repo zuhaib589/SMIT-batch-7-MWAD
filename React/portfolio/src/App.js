15adea4adc9005508ca474b933844e5be12ddb3a
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-22 23:10:25 +0500

    .

--- React/portfolio/src/App.js
+++ React/portfolio/src/App.js
@@ -12,6 +12,8 @@ import Contact from "./components/contact/Contact";
 import "./components/contact/contact.css"
 import QF from "./components/assests/QF.PNG"
 import Cal from "./components/assests/cal.PNG"
+import portfoilo from "./components/assests/portfoilo.PNG"
+
 
 
 
@@ -31,9 +33,9 @@ function App() {
 
     <div id='cards'>
 
-    <Card  title="Quadrant Finder"  imageUrl={QF} desc="Quadrant Finder is a user-friendly application built with Streamlit for an interactive frontend and Python for the underlying logic. " /> 
-    <Card title="Calculator"  imageUrl={Cal} desc="Calculator is a versatile and intuitive application designed with Streamlit for a seamless user experience and Python for its powerful backend. "/>
-    <Card title="pro3"  imageUrl="" desc=""/>
+    <Card  title="Quadrant Finder"  imageUrl={QF} desc="Quadrant Finder is a user-friendly application built with Streamlit for an interactive frontend and Python for the underlying logic. " href="https://github.com/zuhaib589/Python/blob/main/qudrant.py" /> 
+    <Card title="Calculator"  imageUrl={Cal} desc="Calculator is a versatile and intuitive application designed with Streamlit for a seamless user experience and Python for its powerful backend." href="https://github.com/zuhaib589/Python/blob/main/calculator.py" />
+    <Card title="pro3"  imageUrl={portfoilo} desc="" />
     <Card title="pro4"  imageUrl="" desc=""/>
     <Card title="pro5"  imageUrl="" desc=""/>
     <Card title="pro6"  imageUrl="" desc=""/>
