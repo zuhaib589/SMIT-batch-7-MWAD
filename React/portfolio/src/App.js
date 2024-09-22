b3e0b9b3c04d2b27f094e23c023200650c920b94
zuhaib zafar <zuhaibzafar589@gmail.com>
2024-09-22 23:51:18 +0500

    1st portfolio

--- React/portfolio/src/App.js
+++ React/portfolio/src/App.js
@@ -13,6 +13,7 @@ import "./components/contact/contact.css"
 import QF from "./components/assests/QF.PNG"
 import Cal from "./components/assests/cal.PNG"
 import portfoilo from "./components/assests/portfoilo.PNG"
+import food from "./components/assests/food.PNG"
 
 
 
@@ -35,10 +36,10 @@ function App() {
 
     <Card  title="Quadrant Finder"  imageUrl={QF} desc="Quadrant Finder is a user-friendly application built with Streamlit for an interactive frontend and Python for the underlying logic. " href="https://github.com/zuhaib589/Python/blob/main/qudrant.py" /> 
     <Card title="Calculator"  imageUrl={Cal} desc="Calculator is a versatile and intuitive application designed with Streamlit for a seamless user experience and Python for its powerful backend." href="https://github.com/zuhaib589/Python/blob/main/calculator.py" />
-    <Card title="portfolio"  imageUrl={portfoilo} desc="Copying my instructor's portfolio would undermine my personal growth and originality as a developer. " href=""/>
-    <Card title="pro4"  imageUrl="" desc=""/>
-    <Card title="pro5"  imageUrl="" desc=""/>
-    <Card title="pro6"  imageUrl="" desc=""/>
+    <Card title="portfolio"  imageUrl={portfoilo} desc="Copying my instructor's portfolio would undermine my personal growth and originality as a developer. " href="https://github.com/zuhaib589/SMIT-batch-7-MWAD/tree/main/Assignment%205"/>
+    <Card title="Food website"  imageUrl={food} desc="This simple food website is built using Bootstrap for a responsive and clean design. It showcases with a user-friendly interface. " href="https://github.com/zuhaib589/Advance_Wordpress-/tree/main/Bootstrap"/>
+
+  
 
     </div>
 
--- React/portfolio/src/components/header/Header.js
+++ React/portfolio/src/components/header/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
             <>
        <nav className="navbar navbar-expand-lg ">
   <div className="container-fluid ">
-    <a className="navbar-brand " href="#">LOGO</a>
+    <a className="navbar-brand " href="#"></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
